a1d5662fef149e82512c161da1f3d1399619da34
Daniel Olano <daniel@olanod.com>
2020-07-17 06:53:55 +0200

    Allow the creation of entities
    Entities can be queried and react to commands.

--- src/entity.rs
+++ src/entity.rs
@@ -1,8 +1,161 @@
-use crate::EntityId;
-use riker::actors::{Actor, Message};
+use crate::{EntityId, Store, StoreMsg};
+use async_trait::async_trait;
+use chrono::prelude::*;
+use riker::actors::*;
+use std::marker::PhantomData;
 
+/// An Aggregate is the projected data of a series of events of an entity,
+/// given an initial state update events are applied to it until it reaches the desired state.
 pub trait Aggregate: Actor + Message + Default {
     type Update: Message;
     fn id(&self) -> EntityId;
     fn apply_update(&mut self, update: Self::Update);
+}
+
+type StoreRef<A> = ActorRef<StoreMsg<A>>;
+
+/// Implement this trait to allow your entity handle external commands
+#[async_trait]
+pub trait Command: Message {
+    type Agg: Aggregate;
+    async fn handle(self, cx: &Context<CQRS<Self>>, store: &StoreRef<Self::Agg>);
+}
+
+/// For entities that don't require handling commands
+#[derive(Clone, Debug)]
+pub struct NoCmd<A: Aggregate>(PhantomData<A>);
+#[async_trait]
+impl<A: Aggregate> Command for NoCmd<A> {
+    type Agg = A;
+    async fn handle(self, _: &Context<CQRS<Self>>, _: &StoreRef<Self::Agg>) {}
+}
+
+/// Entities
+struct Entity<C: Command> {
+    store: Option<StoreRef<C::Agg>>,
+    //_cmd: PhantomData<C>,
+}
+
+impl<C: Command> Default for Entity<C> {
+    fn default() -> Self {
+        Entity {
+            store: None,
+            //_cmd: PhantomData,
+        }
+    }
+}
+
+impl<C: Command> Actor for Entity<C> {
+    type Msg = CQRS<C>;
+
+    fn pre_start(&mut self, ctx: &Context<Self::Msg>) {
+        self.store = Some(ctx.actor_of::<Store<C::Agg>>(ctx.myself().name()).unwrap());
+    }
+
+    fn recv(&mut self, ctx: &Context<Self::Msg>, msg: Self::Msg, sender: Sender) {
+        match msg {
+            CQRS::Query(q) => self.receive(ctx, q, sender),
+            CQRS::Cmd(c) => {
+                ctx.run(async move {
+                    c.handle(ctx, self.store.as_ref().unwrap()).await;
+                })
+                .unwrap();
+            }
+        };
+    }
+}
+
+impl<C: Command> Receive<Query> for Entity<C> {
+    type Msg = CQRS<C>;
+    fn receive(&mut self, _ctx: &Context<Self::Msg>, q: Query, sender: Sender) {
+        match q {
+            Query::One(id) => self.store.as_ref().unwrap().tell(id, sender),
+            Query::All => self
+                .store
+                .as_ref()
+                .unwrap()
+                .tell(Utc.timestamp(0, 0)..Utc::now(), sender),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum CQRS<C> {
+    Query(Query),
+    Cmd(C),
+}
+impl<C: Command> From<Query> for CQRS<C> {
+    fn from(q: Query) -> Self {
+        CQRS::Query(q)
+    }
+}
+impl<C: Command> From<C> for CQRS<C> {
+    fn from(c: C) -> Self {
+        CQRS::Cmd(c)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum Query {
+    All,
+    One(EntityId),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::store::tests::{Op, TestCount};
+    use crate::Event;
+    use riker_patterns::ask::ask;
+
+    #[derive(Clone, Debug)]
+    enum TestCmd {
+        Create42,
+        Create99,
+        Double(EntityId),
+    }
+    #[async_trait]
+    impl Command for TestCmd {
+        type Agg = TestCount;
+        async fn handle(self, cx: &Context<CQRS<Self>>, store: &StoreRef<Self::Agg>) {
+            match self {
+                TestCmd::Create42 => store.tell(
+                    Event::Create(TestCount {
+                        count: 42,
+                        ..Default::default()
+                    }),
+                    None,
+                ),
+                TestCmd::Create99 => store.tell(
+                    Event::Create(TestCount {
+                        count: 99,
+                        ..Default::default()
+                    }),
+                    None,
+                ),
+                TestCmd::Double(id) => {
+                    let TestCount { count, id } = ask(&cx.system, &store, id).await;
+                    store.tell(Event::Update(id, Op::Add(count)), None)
+                }
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn command_n_query() {
+        let sys = ActorSystem::new().unwrap();
+        let entity = sys.actor_of::<Entity<TestCmd>>("counts").unwrap();
+
+        entity.tell(TestCmd::Create42, None);
+        entity.tell(TestCmd::Create99, None);
+
+        let counts: Vec<TestCount> = ask(&sys, &entity, Query::All).await;
+        assert_eq!(counts[0].count, 42);
+        assert_eq!(counts[1].count, 99);
+
+        let id = counts[0].id();
+        entity.tell(TestCmd::Double(id), None);
+        let result: TestCount = ask(&sys, &entity, Query::One(id)).await;
+        assert_eq!(result.count, 84);
+    }
 }
--- src/store.rs
+++ src/store.rs
@@ -2,6 +2,7 @@ use crate::{Aggregate, EntityId, Event};
 use chrono::prelude::*;
 use riker::actors::*;
 use std::collections::HashMap;
+use std::ops::Range;
 
 /// An actor that handles the persistance of events of entities
 /// using "commits" to track who made a change and why.  
@@ -41,11 +42,17 @@ impl<T: Aggregate> From<Event<T>> for Commit<T> {
 pub enum StoreMsg<T: Aggregate> {
     Commit(Commit<T>),
     Snapshot((EntityId, DateTime<Utc>)),
+    SnapshotList(Range<DateTime<Utc>>),
     Subscribe(EntityId),
 }
 impl<T: Aggregate> From<Event<T>> for StoreMsg<T> {
     fn from(msg: Event<T>) -> Self {
         StoreMsg::Commit(msg.into())
+    }
+}
+impl<T: Aggregate> From<Range<DateTime<Utc>>> for StoreMsg<T> {
+    fn from(range: Range<DateTime<Utc>>) -> Self {
+        StoreMsg::SnapshotList(range)
     }
 }
 impl<T: Aggregate> From<Commit<T>> for StoreMsg<T> {
@@ -71,6 +78,7 @@ impl<T: Aggregate> Actor for Store<T> {
             StoreMsg::Commit(msg) => self.receive(cx, msg, sender),
             StoreMsg::Subscribe(msg) => self.receive(cx, msg, sender),
             StoreMsg::Snapshot(msg) => self.receive(cx, msg, sender),
+            StoreMsg::SnapshotList(msg) => self.receive(cx, msg, sender),
         };
     }
 }
@@ -122,6 +130,13 @@ impl<T: Aggregate> Receive<(EntityId, DateTime<Utc>)> for Store<T> {
     }
 }
 
+impl<T: Aggregate> Receive<Range<DateTime<Utc>>> for Store<T> {
+    type Msg = StoreMsg<T>;
+    fn receive(&mut self, ctx: &Context<Self::Msg>, msg: Range<DateTime<Utc>>, sender: Sender) {
+        todo!()
+    }
+}
+
 impl<T: Aggregate> Receive<EntityId> for Store<T> {
     type Msg = StoreMsg<T>;
     fn receive(&mut self, _cx: &Context<Self::Msg>, _id: EntityId, _sender: Sender) {
@@ -130,13 +145,14 @@ impl<T: Aggregate> Receive<EntityId> for Store<T> {
 }
 
 #[cfg(test)]
-mod tests {
+pub(crate) mod tests {
     use super::*;
     use riker_patterns::ask::ask;
 
     #[derive(Default, Clone, Debug)]
     pub(crate) struct TestCount {
-        count: i16,
+        id: EntityId,
+        pub count: i16,
     }
     #[derive(Clone, Debug)]
     pub(crate) enum Op {
@@ -146,7 +162,7 @@ mod tests {
     impl Aggregate for TestCount {
         type Update = Op;
         fn id(&self) -> EntityId {
-            "123".into()
+            self.id
         }
         fn apply_update(&mut self, update: Self::Update) {
             match update {
@@ -177,5 +193,25 @@ mod tests {
 
         let result: TestCount = ask(&sys, &store, (id, Utc::now())).await;
         assert_eq!(result.count, 42);
+    }
+
+    #[tokio::test]
+    async fn load_list_of_snapshots() {
+        let sys = ActorSystem::new().unwrap();
+        let store = sys.actor_of::<Store<TestCount>>("test-counts").unwrap();
+
+        let before = Utc::now();
+        let some_counter = TestCount {
+            id: "123".into(),
+            count: 42,
+        };
+        store.tell(Event::Create(some_counter), None);
+        store.tell(Event::Create(TestCount::default()), None);
+        store.tell(Event::Create(TestCount::default()), None);
+        store.tell(Event::Update("123".into(), Op::Add(8)), None);
+
+        let result: Vec<TestCount> = ask(&sys, &store, before..Utc::now()).await;
+        assert_eq!(result.len(), 3);
+        assert_eq!(result[0].count, 50);
     }
 }
